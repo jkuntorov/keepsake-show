9afbfe33aa974ed9b5c28891cbe5d931b83c84f2
Julian Kuntorov <julian.kuntorov@gmail.com>
2015-06-17 18:56:42 +0100

    Added sponsor logos. Fixed responsiveness. Updated pictures for Elise and myself.

--- app/index/index.js
+++ app/index/index.js
@@ -12,23 +12,47 @@ angular.module('keepsake.index', ['ngRoute'])
 		$scope.people = data;
 	});
 
+	$scope.courses = [
+		{ id: 'gd', title: 'Graphic Design' },
+		{ id: 'i', title: 'Illustration' }
+	];
+
+	$scope.gd_specialities = [
+		{ id: 'branding', title: 'Branding' },
+		{ id: 'digital', title: 'Digital Design' },
+		{ id: 'editorial', title: 'Editorial Design' },
+		{ id: 'photography', title: 'Photography' }
+	];
+
+	$scope.i_specialities = [
+		{ id: '3d', title: '3D Design' },
+		{ id: 'animation', title: 'Animation' },
+		{ id: 'digital', title: 'Digital Illustration' },
+		{ id: 'print', title: 'Print Illustration' }
+	];
+
+	$scope.specialities = [];
+
 	$scope.clearFilters = function() {
 		$scope.search = {};
 	};
 
 	$scope.$watch('search.course', function(newValue, oldValue) {
+		console.log($scope.search);
 		$rootScope.search = $scope.search;
 		if ($scope.search) {
-			if ($scope.search.course == "") { $scope.search = {} };
+			if ($scope.search.course == 'gd') { $scope.specialities = $scope.gd_specialities; };
+			if ($scope.search.course == 'i') { $scope.specialities = $scope.i_specialities; };
+			if ($scope.search.course == null) { $scope.search = {} };
 			if ($scope.search.speciality && (newValue != oldValue)) { $scope.search.speciality = "" };
 		}
-		console.log("new " + newValue);
-		console.log("old " + oldValue);
 	});
 
 	$scope.$watch('search.speciality', function() {
 		$rootScope.search = $scope.search;
-		console.log($rootScope.search);
+		if ($scope.search) {
+			if ($scope.search.speciality == null) { $scope.search.speciality = ""; };
+		}
 	});
 
 	// reapply filter to the listing
